5bd1aa46fb5649fe7f89980e19d60ad8340fa888
SimplyPancake <danielduikvis@gmail.com>
2018-03-12 14:29:58 +0100

    More thingggg

--- mousePos/ldBarScript.js
+++
@@ -1,3 +0,0 @@
-var bar1 = new ldBar("#ldBarId");
-var bar2 = document.getElementById('ldBarId').ldBar;
-bar1.set(60);
--- mousePos/script.js
+++ mousePos/script.js
@@ -1,8 +1,30 @@
-const coords = [[92, 81],  [81, 47], [81, 03]];
-var touchX = 50, touchY = 50;
-coords[[92,81]] = "https://goo.gl/8cm9HU";
-coords[[81,47]] = "https://goo.gl/azAT2j";
-coords[[81,03]] = "https://goo.gl/htsTmk";
+var text = "gone"
+document.getElementById('neushoornPlaatje').style = " display: none;"
+document.getElementById('welkomTekst').style = "display: none;"
+
+setTimeout(function() {
+  document.getElementById('neushoornPlaatje').style = "display: block;"
+  document.getElementById('ldBarId').style = " display: none;"
+  document.getElementById('welkomTekst').style = " display: block;"
+  text = "here"
+}, 1100);
+
+function putAwayText(){
+  if (text == "here") {
+  document.getElementById('welkomTekst').style = "display: none;"
+  }
+}
+
+const coords = [
+  [92, 81],
+  [81, 47],
+  [81, 03]
+];
+var touchX = 50,
+  touchY = 50;
+coords[[92, 81]] = "https://goo.gl/8cm9HU";
+coords[[81, 47]] = "https://goo.gl/azAT2j";
+coords[[81, 03]] = "https://goo.gl/htsTmk";
 
 console.log("Please ignore the errors, HTML is being a retard again...");
 // ----------------------------------------------------------------------------------------------------------------
@@ -10,34 +32,35 @@ console.log("Please ignore the errors, HTML is being a retard again...");
 
 // ----------------------------------------------------------------------------------------------------------------
 function logCoords() {
-    var cX = event.clientX; var cY = event.clientY;
-    //Krijg variabelen als 'clientsizeX' en 'clientsizeY'
-    //Zie: https://i.stack.imgur.com/6xPdH.png    We gaan 'windowWidth' gebruiken
-    var wWidth = window.innerWidth;
-    var wHeight = window.innerHeight;
-    //Nu krijg ik ook de windowWidth en Height. Hier wordt de app op gebaseerd.
-    var coords1 = "client - X: " + cX + ", Y coords: " + cY;
-    var coords3 = "window size: - X: " + wWidth + ", Y: " + wHeight;
-    var percentX = Math.round(cX / wWidth * 100);
-    var percentY = Math.round(cY / wHeight * 100);
-    //Het percentage is de relatie tussen de X  en Y coords van het browser-window en de size van het browser-window
+  var cX = event.clientX;
+  var cY = event.clientY;
+  //Krijg variabelen als 'clientsizeX' en 'clientsizeY'
+  //Zie: https://i.stack.imgur.com/6xPdH.png    We gaan 'windowWidth' gebruiken
+  var wWidth = window.innerWidth;
+  var wHeight = window.innerHeight;
+  //Nu krijg ik ook de windowWidth en Height. Hier wordt de app op gebaseerd.
+  var coords1 = "client - X: " + cX + ", Y coords: " + cY;
+  var coords3 = "window size: - X: " + wWidth + ", Y: " + wHeight;
+  var percentX = Math.round(cX / wWidth * 100);
+  var percentY = Math.round(cY / wHeight * 100);
+  //Het percentage is de relatie tussen de X  en Y coords van het browser-window en de size van het browser-window
 
-    console.clear();
-    console.log(coords1);
-    console.log(coords3);
-    console.log("X: " + percentX + " Y: " + percentY);
-    touchX = percentX;
-    touchY = percentY;
+  console.clear();
+  console.log(coords1);
+  console.log(coords3);
+  console.log("X: " + percentX + " Y: " + percentY);
+  touchX = percentX;
+  touchY = percentY;
 }
 // ----------------------------------------------------------------------------------------------------------------
-function changePic(){
+function changePic() {
   let closest = [null, null];
   let distance = 142; //Square root of 100^2 + 100^2
 
-  for(const [x, y] of coords){
+  for (const [x, y] of coords) {
     let d = Math.sqrt((touchX - x) ** 2 + (touchY - y) ** 2);
     // let d = Math.sqrt((Math.sqrt(touchX - x), 2) + Math.sqrt((touchY - y), 2));
-    if(d < distance){
+    if (d < distance) {
       closest = [x, y];
       distance = d;
     }
