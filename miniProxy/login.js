17016aab89731a92842c24dd428ea89ec56fc3c5
SimplyPancake <danielduikvis@gmail.com>
2018-03-13 10:35:11 +0100

    Added proxy login

---
+++ miniProxy/login.js
@@ -0,0 +1,24 @@
+var gebruikersnamen = ["Danian", "root"];
+var wachtwoorden = [];
+wachtwoorden["root"] = " ,."
+wachtwoorden["Danian"] = "22"
+
+var attempts = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  var a = gebruikersnamen.indexOf(username)
+  console.log(a);
+
+  if ( a !== "-1" && password == wachtwoorden[username]){
+  console.log("Login successfully");
+  alert("Login successfull!");
+  window.location.replace("miniProxy.php")
+  return false;
+} else {
+  alert("Login unsuccesfull!")
+  location.reload();
+  }
+}
