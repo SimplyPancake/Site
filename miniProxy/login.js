462e55aac0be20feae529bb30b3325b0ef503694
SimplyPancake <danielduikvis@gmail.com>
2018-03-13 11:38:54 +0100

    Added Deriz

--- miniProxy/login.js
+++ miniProxy/login.js
@@ -1,6 +1,7 @@
-var gebruikersnamen = ["root"];
+var gebruikersnamen = ["root", "Deriz"];
 var wachtwoorden = [];
 wachtwoorden["root"] = "root"
+wachtwoorden["Deriz"] = "Deriz"
 
 var attempts = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
